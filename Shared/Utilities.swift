fe67fee1e6a0e0392adbcdb071935b3f5ef56e58
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-20 10:08:49 +0900

    Set the color palette

---
+++ Shared/Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  Utilities.swift
+//  InteractiveRatingComponent (iOS)
+//
+//  Created by Jinwook Kim on 2022/03/20.
+//
+
+import SwiftUI
+
+struct Palette {
+    static let backgroundColor: Color = Color(red: 20, green: 21, blue: 24)
+    static let cardColor: Color = Color(red: 33, green: 38, blue: 47)
+    static let buttonColor: Color = Color(red: 41, green: 46, blue: 51)
+    static let submitColor: Color = Color(red: 236, green: 126, blue: 53)
+    static let textColor: Color = Color(red: 131, green: 136, blue: 145)
+}
