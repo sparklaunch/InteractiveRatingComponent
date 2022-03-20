a6c26bc26ff7c9aa7e5f2b3612eed16201c535d2
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-20 10:26:59 +0900

    Complete the star button.

--- Shared/Components/StarButton.swift
+++ Shared/Components/StarButton.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 struct StarButton: View {
     var body: some View {
-        Text("Placeholder")
+        ZStack {
+            Palette.buttonColor
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+            Image("Star")
+                .resizable()
+                .frame(width: 16, height: 16)
+        }
+        .shadow(radius: 5)
     }
 }
