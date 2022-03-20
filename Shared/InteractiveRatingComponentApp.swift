e1dfe721ea79afcadb5ffb382669eb11b05699f7
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-20 09:39:56 +0900

    Initial Commit

---
+++ Shared/InteractiveRatingComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveRatingComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveRatingComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
