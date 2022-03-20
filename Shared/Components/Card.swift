6afabc0e69ec88db4f5be471f9b4fb74b7d55583
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-20 10:39:27 +0900

    Add the texts.

--- Shared/Components/Card.swift
+++ Shared/Components/Card.swift
@@ -11,9 +11,18 @@ struct Card: View {
     var body: some View {
         ZStack {
             Palette.cardColor
-            VStack {
+            VStack(alignment: .leading) {
                 StarButton()
+                Text("How did we do?")
+                    .font(.custom("Overpass", size: 24))
+                    .font(.title.weight(.heavy))
+                    .foregroundColor(.white)
+                Text("Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!")
+                    .font(.custom("Overpass", size: 16))
+                    .font(.body.weight(.semibold))
+                    .foregroundColor(Palette.textColor)
             }
+            .padding(30)
         }
         .padding(.horizontal, 30)
     }
